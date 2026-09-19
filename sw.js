686624fb2da07e2701234696230eee0ef1661fa7
beaconhillacademy123 <beaconhillacademykano@gmail.com>
2026-09-19 22:56:49 +0100

    Refresh Quizy PWA icon cache

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE='quizy-shell-v1';
-const CORE=['/','/index.html','/manifest.json','/icon-512.png'];
+const CACHE='quizy-shell-v2';
+const CORE=['/','/index.html','/manifest.json','/icon-192.png','/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
   const u=new URL(e.request.url);
   if(e.request.method!=='GET') return;
