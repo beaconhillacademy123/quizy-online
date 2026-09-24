e43623c410eea9b1fcf16340c9d70401c13e72c2
beaconhillacademy123 <beaconhillacademykano@gmail.com>
2026-09-24 10:41:53 -0700

    Refresh Quizy app shell after startup fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='quizy-shell-v7';
+const CACHE='quizy-shell-v8';
 const CORE=['/','/index.html','/manifest.json','/icon-192.svg','/icon-512.svg'];
 
 self.addEventListener('install',e=>{
