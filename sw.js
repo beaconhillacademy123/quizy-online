2f9fa9cc5331cf2f2b231dde95025829af456bb7
beaconhillacademy123 <beaconhillacademykano@gmail.com>
2026-09-24 06:44:36 -0700

    Refresh Quizy app shell for redesign updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='quizy-shell-v6';
+const CACHE='quizy-shell-v7';
 const CORE=['/','/index.html','/manifest.json','/icon-192.svg','/icon-512.svg'];
 
 self.addEventListener('install',e=>{
